2744c0d0ea50198e629eb7147e3ef6438327fc48
Damien MAILHEBIAU <damienmailhebiau@gmail.com>
2025-01-07 15:54:25 +0100

    fix: swagger

--- src/main/java/com/jamify/uaa/config/security/SecurityConfig.java
+++ src/main/java/com/jamify/uaa/config/security/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/", "/auth/**", "/login/**", "/oauth2/**", "/oauth/.well-known/jwks.json").permitAll()
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**", "/v3/api-docs").permitAll()
                         .requestMatchers("/api/v1/auth/refresh-jwt-token").permitAll()
                         .requestMatchers("/api/v1/user").hasAuthority(Role.USER.getValue())
                         .anyRequest().authenticated()
